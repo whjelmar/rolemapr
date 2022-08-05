e90be3d4c3adae2f7a5b83bf5223137802a7a519
Walter Hjelmar <walter.hjelmar@synechron.com>
2022-08-04 23:32:12 -0400

    feat: usethis::use_package_doc()

--- R/rolemapr.R
+++
@@ -1,6 +0,0 @@
-#' @docType package
-#' @name rolemapr-package
-#' @aliases rolemapr Rolemapr Rolemapr-package
-#' @title Role Mapper
-#' @description A package to produce roles and responsibility maps.
-NULL
